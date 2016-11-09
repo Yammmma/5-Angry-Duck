908f0c49e40624006f04a080c82f3649a06b615e
Yuma S <yuma@Yumas-MacBook-Air.local>
2016-11-09 16:56:49 +1100

    Initial commit

---
+++ Angry Duck - 2/GameScene.swift
@@ -0,0 +1,104 @@
+//
+//  GameScene.swift
+//  Angry Duck - 2
+//
+//  Created by Yuma Soerianto on 7/11/16.
+//  Copyright © 2016 Yuma Soerianto. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    var duck: SKSpriteNode!
+    
+    var originalDuckPos: CGPoint!
+    var hasGone = false
+    
+    override func didMove(to view: SKView) {
+        duck = childNode(withName: "duck") as! SKSpriteNode
+        
+        originalDuckPos = duck.position
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !hasGone {
+            if let touch = touches.first {
+                let touchLocation = touch.location(in: self)
+                let touchedWhere = nodes(at: touchLocation)
+                
+                if !touchedWhere.isEmpty {
+                    for node in touchedWhere {
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == duck {
+                                duck.position = touchLocation
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !hasGone {
+            if let touch = touches.first {
+                let touchLocation = touch.location(in: self)
+                let touchedWhere = nodes(at: touchLocation)
+                
+                if !touchedWhere.isEmpty {
+                    for node in touchedWhere {
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == duck {
+                                duck.position = touchLocation
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !hasGone {
+            if let touch = touches.first {
+                let touchLocation = touch.location(in: self)
+                let touchedWhere = nodes(at: touchLocation)
+                
+                if !touchedWhere.isEmpty {
+                    for node in touchedWhere {
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == duck {
+                                let dx = -(touchLocation.x - originalDuckPos.x)
+                                let dy = -(touchLocation.y - originalDuckPos.y)
+                                let impulse = CGVector(dx: dx, dy: dy)
+                
+                                duck.physicsBody?.applyImpulse(impulse)
+                                duck.physicsBody?.applyAngularImpulse(-0.01)
+                                duck.physicsBody?.affectedByGravity = true
+                                
+                                hasGone = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if let duckVelocity = duck.physicsBody?.velocity, let duckAngleVlcity = duck.physicsBody?.angularVelocity {
+            if duckVelocity.dx <= 0 && duckVelocity.dy <= 0 && duckAngleVlcity <= 0 && hasGone {
+                duck.physicsBody?.affectedByGravity = false
+                duck.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                duck.physicsBody?.angularVelocity = 0
+                duck.zRotation = 0
+                duck.position = originalDuckPos
+                
+                hasGone = false
+            }
+        }
+    }
+}
---
+++ Angry Duck - 2/GameViewController.swift
@@ -0,0 +1,55 @@
+//
+//  GameViewController.swift
+//  Angry Duck - 2
+//
+//  Created by Yuma Soerianto on 7/11/16.
+//  Copyright © 2016 Yuma Soerianto. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = GameScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = false
+            view.showsNodeCount = false
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
